36e54ef7c18e9dae7aafe0b2a10c91f132b2675b
IrinaVolkhonskaya <irina.volhonskaya@gmail.com>
2019-04-14 14:55:34 +0300

    Practise 6

---
+++ JSPractise/JS_Module1_Variable/indexPractise.6.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const num = prompt("Введите число от 1 до 5");
+
+if (num === null) {
+  alert("Очень жаль, приходите еще!");
+}
+switch (num) {
+  case "1":
+    alert("Каталог хостелов");
+    break;
+  case "2":
+    alert("Каталог бюджетных отелей");
+    break;
+  case "3":
+    alert("Каталог отелей ***");
+    break;
+  case "4":
+    alert("Каталог отелей ****");
+    break;
+  case "5":
+    alert("Каталог лучших отелей");
+    break;
+  default:
+    alert("Введите число");
+}
